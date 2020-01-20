1b58fb72964c5ffb72a03fbf48b34386522b8448
HYQ <Chelsea@promote.cache-dns.local>
2020-01-20 23:43:10 +0800

    add icons

---
+++ src/pages/home/components/Icons.vue
@@ -0,0 +1,126 @@
+<template>
+  <div class="icons">
+    <swiper :options="swiperOption">
+      <swiper-slide v-for="(page, index) of pages" :key="index">     
+        <div 
+          class="icon" 
+          v-for="item of page" 
+          :key="item.id"
+        >
+          <div class="icon-img">
+            <img class="icon-img-content" :src="item.imgUrl">
+          </div>
+          <p class="icon-desc">{{item.desc}}</p>
+        </div> 
+      </swiper-slide>
+      <div class="swiper-pagination"  slot="pagination"></div>
+   </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeIcons',
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+      },
+      iconList:[{
+        id: '0001',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+        desc: '景点门票'
+      },{
+        id: '0002',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/fc/b10a6b2e4f0fe102.png',
+        desc: '万隆滑雪'
+      },{
+        id: '0003',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/6c/9e54a8540fee0102.png',
+        desc: '故宫'
+      },{
+        id: '0004',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/fc/b10a6b2e4f0fe102.png',
+        desc: '静之湖滑雪'
+      },{
+        id: '0005',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1804/5a/13ceb38dcf262f02.png',
+        desc: '一日游'
+      },{
+        id: '0006',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/67/9a1678221b8e0e02.png',
+        desc: '古北水镇'
+      },{
+        id: '0007',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/ab/6f7d6e44963c9302.png',
+        desc: '泡温泉'
+      },{
+        id: '0008',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/47/c2b659e048b11602.png',
+        desc: '中国马镇'
+      },{
+        id: '0009',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/fc/b10a6b2e4f0fe102.png',
+        desc: '北京滑雪'
+      },{
+        id: '0010',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/fc/b10a6b2e4f0fe102.png',
+        desc: '陶然亭冰雪'
+      }]
+    }
+  },
+  computed: {
+    pages () {
+      const pages = []
+      this.iconList.forEach((item,index) => {
+        const page = Math.floor(index / 8)
+        if (!pages[page]) {
+          pages[page] = []
+        }
+        pages[page].push(item)
+      })
+      return pages
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/variables.styl'
+  @import '~styles/mixins.styl'
+  .icons >>> .swiper-pagination-bullet-active 
+    background: $bgColor
+  .icons >>> .swiper-container
+    height: 0
+    padding-bottom: 56%
+  .icons
+    overflow: hidden
+    width: 100%
+    height: 0
+    padding-bottom: 56%
+    .icon
+      position: relative
+      float: left
+      width: 25%
+      height: 0
+      padding-bottom: 25%
+      .icon-img
+        position: absolute
+        top: .2rem
+        right: .2rem
+        left: .2rem
+        bottom: .6rem 
+        text-align: center
+        .icon-img-content
+          height: 100%
+      .icon-desc
+        position: absolute
+        right: 0
+        left: 0
+        bottom: 0.2rem
+        height: .4rem
+        line-height: .4rem
+        text-align: center
+        corlor: $darkTextColor
+        ellipsis()
+</style>
