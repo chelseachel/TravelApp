665f322caa3ad10a068a568c0a17586761a4db49
chelseachel <cheecheese@outlook.com>
2020-06-07 18:58:05 +0800

    config

--- src/pages/city/components/Search.vue
+++ src/pages/city/components/Search.vue
@@ -56,6 +56,7 @@ export default {
           })
         }
         this.list = result
+        this.timer = null
       }, 100)
     }
   },
--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,9 @@
-const webpack = require('webpack')
 const path = require('path');
 
 function resolve(dir) {
     return path.join(__dirname, dir)
 }
 module.exports = {
-    // 基本路径
-    publicPath: './',
-    // 转达ajax数据请求
-    
      /* webpack-dev-server 相关配置 */
     devServer: {
         port: 8080,
@@ -22,21 +17,12 @@ module.exports = {
             }
         }
     },
-    // webpack配置
-    configureWebpack: {
-        plugins: [
-            new webpack.ProvidePlugin({
-                jQuery: 'jquery',
-                $: 'jquery'
-            })
-        ]
-    },
     /* 生产环境是否生成 sourceMap 文件，false将提高构建速度 */
     productionSourceMap: false,
     /* 代码保存时是否进行eslint检测 */
     lintOnSave: false,
     // webpack配置
-    // see https://github.com/vuejs/vue-cli/blob/dev/docs/webpack.md
+    // ref https://github.com/vuejs/vue-cli/blob/dev/docs/webpack.md
     chainWebpack: config => {
         config.entry.app = ["babel-polyfill", resolve('src/main.js')],
         config.resolve.alias
